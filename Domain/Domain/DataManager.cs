7fa41f966c546fa623bd50d8b9f99cf770a77f1a
van8tka <van8tka@tut.by>
2016-03-10 22:04:04 +0300

    1
    Начало создание проекта

---
+++ Domain/Domain/DataManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+   public class DataManager
+    {
+        //в этом классе происходит обмен данными его будем внедрять в конструктор каждого класса
+    }
+}
