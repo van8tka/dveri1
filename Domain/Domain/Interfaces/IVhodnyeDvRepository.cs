d7a69df13c3ff41f41f1831def3e79ba152b2e70
van8tka <van8tka@tut.by>
2016-03-10 22:13:26 +0300

    2
    проект создание интерфейса на Vhodnye dveri

---
+++ Domain/Domain/Interfaces/IVhodnyeDvRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DALdv1;
+
+namespace Domain.Interfaces
+{
+   public interface IVhodnyeDvRepository
+    {
+        IEnumerable<VhodnyeDveri> GetVhodnyeDv();
+        VhodnyeDveri GetVhodnyeDvById(int id);
+        void CreateVhodnyeDv(int id, string naz, string pr, string strpr, string cvet, string napoln, string yplotnit, int tmet, string furn, string petli, string osn, string ovn, int tdpol, int cena, int skidka, int csskid, string opis, bool publ);
+        void SaveVhodnyeDv(VhodnyeDveri temp);
+        void DeleteVhodnyeDv(int id);
+
+    }
+}
